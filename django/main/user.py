c2c7cfcb4bbf8cd0e036c8cf6c4726fdf9795e5c
Stephane Collot <stephane.collot@gmail.com>
2015-04-26 14:18:38 -0400

    User class + serialization + index base

---
+++ django/main/user.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.shortcuts import render_to_response
+from django.conf import settings
+import json
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class Competency(object):
+  def __init__(self, text, score):
+    #logger.info('User' + str(id))
+    self.text = text
+    self.score = 0
+    self.removeHtml = '<a href="#" onClick="remove('+str(text)+'); return false;"><img src="s/remove.png" alt="Remove" class="removeBtn" ></a>'
+    
+  def toJSON(self):
+    return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
+class User(object):
+  def __init__(self, id):
+    #logger.info('User' + str(id))
+    self.id = id
+    self.competencies = []
+    
+  def toJSON(self):
+    return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
--- django/main/views.py
+++ django/main/views.py
@@ -4,6 +4,7 @@ from django.template import Context, loader
 from django.shortcuts import render_to_response
 from django.conf import settings
 import json
+from user import User, Competency
 
 import logging
 logger = logging.getLogger(__name__)
@@ -50,8 +51,18 @@ def remove(request, text):
   request.session.modified = True
     
   print request.session['competencies']
-
+  
+  user1 = User(1)
+  user1.competencies.append(Competency('c++',11))
+  user1.competencies.append(Competency('java',12))
+  user2 = User(2)
+  user2.competencies.append(Competency('c++',21))
+  user2.competencies.append(Competency('java',25))
+  
   result = []
+  result.append(user1)
+  result.append(user2)
+  
   result_json = json.dumps(result, default=lambda o: o.__dict__)
   response = HttpResponse(result_json, content_type='application/json')
   response.__setitem__("Access-Control-Allow-Origin", "*") #enables CORS (required to use json)
